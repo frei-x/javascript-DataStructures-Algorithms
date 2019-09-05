65a38c2a7344d680a1d9b7176310fea3c7a02f08
心远地自偏 <javascript.h@qq.com>
2019-09-05 23:17:43 +0800

    使用CommonJS规范/检测可迭代对象

--- Array/map-x.js
+++ Array/map-x.js
@@ -1,29 +1,22 @@
-Array.prototype.mapX = function (callback) {
-  console.log(this);
-  if (!Array.isArray(this)) {
-    throw new TypeError(this + " 不是数组")
+let tool = require("../tool/tool.js");
+Array.prototype.mapX = function(callback) {
+  if (!tool.isIterable(this)) {
+    throw new TypeError(this + " 不是可迭代对象");
   }
   if (this.length <= 0) {
-    throw new TypeError(this + " 为空")
+    throw new TypeError(this + " 为空");
   }
   for (let i = 0; i < this.length; i++) {
     this[i] = callback(this[i], i);
   }
-}
-var number = [2, 4, 6, 8, 10]
+};
+var number = [2, 4, 6, 8, 10];
+
 number.mapX((item, index) => {
   if (index < 2) {
-    return item + 1
+    return item + 2;
   }
-  return item * item
-
-})
+  return item * item;
+});
 console.log(number);
-// 检测是否为可迭代对象:
-function isIterable(obj) {
-  // checks for null and undefined
-  if (obj == null) {
-    return false;
-  }
-  return typeof obj[Symbol.iterator] === 'function';
-}
+debugger;
